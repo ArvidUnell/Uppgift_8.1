ab6b6681f0b7ab37ad5e1b6f424b61da422afc06
ArvidUnell <arun06001@utb.vaxjo.se>
2024-04-10 13:31:47 +0200

    Add project files.

---
+++ Uppgift_8.1/Bil.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_8._1
+{
+    internal class Bil
+    {
+        private string märke;
+
+        public string Märke
+        {
+            get
+            {
+                return märke;
+            }
+            set
+            {
+                märke = value;
+            }
+        }
+
+
+        private string modell;
+
+        public string Modell
+        {
+            get
+            {
+                return modell;
+            }
+            set
+            {
+                modell = value;
+            }
+        }
+
+
+        private int årsmodell;
+
+        public int Årsmodell
+        {
+            get
+            {
+                return årsmodell;
+            }
+            set
+            {
+                årsmodell = value;
+            }
+        }
+
+
+        private int vikt;
+
+        public int Vikt
+        {
+            get
+            {
+                return vikt;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    vikt = 0;
+                }
+                else
+                {
+                    vikt = value;
+                }
+            }
+        }
+
+
+        private int topphastighet;
+
+        public int Topphastighet
+        {
+            get
+            {
+                return topphastighet;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    topphastighet = 0;
+                }
+                else
+                {
+                    topphastighet = value;
+                }
+            }
+        }
+
+
+
+        public void SkrivInfo()
+        {
+            Console.WriteLine("Märke: " + märke);
+            Console.WriteLine("Modell: " + modell);
+            Console.WriteLine("Årsmodell: " + årsmodell);
+            Console.WriteLine($"Vikt: {vikt} kg");
+            Console.WriteLine($"Topphastighet: {topphastighet} km/h");
+        }
+    }
+}
---
+++ Uppgift_8.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Uppgift_8._1;
+
+namespace Uppgift_8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bil bil1 = new Bil();
+            bil1.Märke = "Volvo";
+            bil1.Modell = "V70";
+            bil1.Årsmodell = 1998;
+            bil1.Vikt = 1200;
+            bil1.Topphastighet = 160;
+
+            bil1.SkrivInfo();
+        }
+    }
+}
